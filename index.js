bc4a022756308ea7647f4913bef0127e39457960
LJFrancis <joe.mengis@gmail.com>
2020-08-07 11:40:33 -0700

    add class document

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const Database = require("./js/dbclass");
 
 var connection = mysql.createConnection({
     host: "localhost",
